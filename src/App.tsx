840470f0005d70c9ba44d3b0d193b9703ee298c4
ahiru <wellingtonnmaeda0524@gmail.com>
2022-12-18 08:45:23 +0900

    デプロイテストコミット

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ export const App: React.FC = () => {
     <div className='App'>
       <header className='App-header'>
         <img src={logo.toString()} className="App-logo" alt="logo" />
+        <p>Successfully Deployed <code>A React application</code> to Netlify with CircleCI</p>
       </header>
-      <main className='App-main'>
+      {/* <main className='App-main'>
         <p>this is main content</p>
       </main>
       <footer className='App-footer'>
         <small>this is sample footer text</small>
-      </footer>
+      </footer> */}
     </div>
   );
 };
