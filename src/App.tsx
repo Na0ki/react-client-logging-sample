5928d8bf431c23e269ab5f87eeed4ea8e77c54c5
ahiru <wellingtonnmaeda0524@gmail.com>
2022-12-18 23:57:55 +0900

    sentryテスト用コンポーネント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SomeComponent } from './components/some/some.component';
+
+const getRandomInt = (min: number = 0, max: number = 100) => {
+  if (min > max) {
+      throw new Error('min must be less or equals to max');
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const generateRandomItems = () => {
+  return new Array(5).fill(0).reduce((prev, _, index) => {
+    const id = index + 1;
+    const value = getRandomInt();
+    prev.push({id, value});
+    return prev;
+  }, []);
+};
 
 export const App: React.FC = () => {
+  const items = generateRandomItems();
+
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo.toString()} className="App-logo" alt="logo" />
         <p>Successfully Deployed <code>A React application</code> to Netlify with CircleCI</p>
+        <div>
+          <SomeComponent items={items}></SomeComponent>
+        </div>
       </header>
-      {/* <main className='App-main'>
-        <p>this is main content</p>
-      </main>
-      <footer className='App-footer'>
-        <small>this is sample footer text</small>
-      </footer> */}
     </div>
   );
 };
---
+++ src/components/some/some.component.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+type SomeItemProps = {
+    id: string;
+    value: number;
+};
+
+type SomeProps = {
+    items: {id: string, value: number}[];
+};
+
+const SomeItemComponent: React.FC<SomeItemProps> = (props) => {
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        if (props.value % 2 === 1) {
+            throw new Error(`%{value} is not even number`);
+        }
+    };
+
+    return (
+        <li key={props.id}>
+            <button type='button' onClick={handleClick}>{props.value}</button>
+        </li>
+    );
+};
+
+export const SomeComponent: React.FC<SomeProps> = (props) => {
+    return (
+        <ul>
+            {props.items.map(item => <SomeItemComponent {...item}></SomeItemComponent>)}
+        </ul>
+    );
+};
