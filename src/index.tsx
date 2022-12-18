6cefb214663e14444238cabf22a8ef2caa19fcfa
ahiru <wellingtonnmaeda0524@gmail.com>
2022-12-18 23:04:03 +0900

    sentryのクライアントコード追加

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://3c871f311973466e9f8bf8ecfb09e8a0@o4504305357488128.ingest.sentry.io/4504350363222016",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
